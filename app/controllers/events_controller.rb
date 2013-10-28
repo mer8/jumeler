b4f8f6844e7fbd5e1225be1271fb6693004514ae
Meredith Bryan <merbryan@gmail.com>
2013-10-28 15:22:17 -0700

    Messed with some CSS/SASS

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,10 +12,25 @@ class EventsController < ApplicationController
 	end
 
 	def create
+		# I commented below out bc. I was tring to make it so you have to fill in the fields otherwise you'll get an error. I can't get it to work
+		# if params[:date][:time][:name].present?
+
 		e = Event.create(params[:event].permit(:date, :time, :name))
 		redirect_to events_url
+		# I comment this below out because it was redirecting to a bad page
 		# :action => "show" , :id =>@event._id
+
+
+
+		# I commented below out bc. I was tring to make it so you have to fill in the fields otherwise you'll get an error. I can't get it to work
+		# else
+		# 	flash.now.alert = "Unable to create event. Please try again."
+		# 	render :new
+		# end
 	end
+
+
+
 
 	def destroy
 		Event.find(params[:id]).destroy
